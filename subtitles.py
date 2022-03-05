95a0e8392a00059c432fd8f03747074eee4cc0f4
anoushkaramel <72388086+anoushkaramel@users.noreply.github.com>
2022-03-05 17:19:48 +0530

    Create subtitles.py

---
+++ subtitles.py
@@ -0,0 +1,27 @@
+import speech_recognition as sr 
+import moviepy.editor as mp
+import webbrowser as wb
+
+clip = mp.VideoFileClip(r"/Users/anoushkadixit/Downloads/meet.mov") 
+clip.audio.write_audiofile(r"/Users/anoushkadixit/Downloads/converted.wav")
+
+audio = sr.AudioFile("/Users/anoushkadixit/Downloads/converted.wav")
+
+def extractaudio(audio):
+    r = sr.Recognizer()
+    
+    with audio as source: 
+        audio_file = r.record(source)
+        
+    result = r.recognize_google(audio_file, language = 'en-IN', show_all = True)
+    
+    with open('/Users/anoushkadixit/Downloads/recognized.txt',mode ='w') as file: 
+        file.write("Recognized Speech:") 
+        file.write("\n")
+        for i in result:
+            file.write(i) 
+        print("transcript ready")
+    return
+
+
+extractaudio(audio)
